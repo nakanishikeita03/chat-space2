c358ebf31256d2563b1bda246a7c46d8196fa916
nakanishikeita03 <0816724keita@gmail.com>
2019-08-11 20:37:23 +0900

    create_model

--- db/migrate/20190811094846_create_groups.rb
+++ db/migrate/20190811094846_create_groups.rb
@@ -1,8 +1,10 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-
+      t.string :name, null: false
+      t.index :name, unique: true
       t.timestamps
+      add_column :groups, :name, :string, :null=>false
     end
   end
 end
