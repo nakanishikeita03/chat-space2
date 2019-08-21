ae8c8bd45b8cf98d4706e0b537379b3727622aa7
nakanishikeita03 <0816724keita@gmail.com>
2019-08-21 12:15:46 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,8 @@
+
 $(function(){
   function buildHTML(message){
-    var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
-     var html = `<div class=message>
+    var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`;
+     var html = `<div class="message" data-message-id='${message.id}'>
                     <div class="upper-message">
                       <div class="upper-message__name">
                       ${message.user_name}
@@ -43,4 +44,29 @@ $(function(){
       alert('error');
     })
   })
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $(".message:last").data("message-id");
+    $.ajax({
+      url: `api/messages`,
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function (messages) {
+      var insertHTML ='';
+      messages.forEach(function (message) {
+        insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight},'fast');
+    });
+  })
+    .fail(function () {
+      alert('自動更新に失敗しました');
+
+    });
+  }
+};
+setInterval(reloadMessages, 5000);
+
 });
